d4c4e7c94518ee96f22a260ae527f0d1f36b6c47
umair-salman <32292175+umair-salman@users.noreply.github.com>
2021-03-23 14:29:14 +0800

    Add files via upload

---
+++ Common/Utility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Common
+{
+    public static class Utitliy
+    {
+        const double minDeliveryCost= 5.95;
+        const double deliveryFeeThreshold = 20.0;
+        public static double ApplyGST(double cost)
+        {          
+
+            try
+            {
+                if (!(double.IsNaN(cost)))
+
+                {
+
+                    cost += cost * 0.1;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return cost;
+
+        }
+
+        public static double AddDeliveryFee(double cost)
+        {
+            try
+            {
+                if (!(double.IsNaN(cost)) && cost < deliveryFeeThreshold)
+
+                {
+
+                    cost = cost + minDeliveryCost;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return cost;
+
+        }
+
+    }
+}
